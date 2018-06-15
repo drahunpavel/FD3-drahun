d92267a898aa1a2041744e7df90ea6204daa7a1e
drahunpavel <drahunpavel@gmail.com>
2018-06-15 20:56:24 +0300

    ps

---
+++ filter/filter.js
@@ -0,0 +1,10 @@
+var FilterComponent = React.createClass({
+
+    displayName:"FilterComponent",
+
+    //Описание компонента
+    propTypes:{
+        default: React.PropTypes.arrayOf(React.PropTypes.string) //React.PropTypes.arrayOf(React.PropTypes.тип) — массив значений указанного типа
+    },
+
+});
