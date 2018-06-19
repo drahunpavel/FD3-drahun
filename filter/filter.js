f7622ddb15e0e2d9bc8f180a4d2adbe60f15a7a5
drahunpavel <drahunpavel@gmail.com>
2018-06-19 05:49:41 +0300

    completed

--- filter/filter.js
+++ filter/filter.js
@@ -1,10 +1,56 @@
-var FilterComponent = React.createClass({
+var FullList = React.createClass({
 
-    displayName:"FilterComponent",
+    displayName: "FullList",
 
-    //Описание компонента
-    propTypes:{
-        default: React.PropTypes.arrayOf(React.PropTypes.string) //React.PropTypes.arrayOf(React.PropTypes.тип) — массив значений указанного типа
+    //Описание React-компонента
+    propTypes: {
+        fullList: React.PropTypes.arrayOf(React.PropTypes.string)
     },
 
+    // Инициализилуем значения стейтов
+    getInitialState: function () {
+        return {
+            stateFullList: this.props.fullList.slice(),
+            stateFullListSort: this.props.fullList.slice().sort(),
+            stateInputText: '',
+            checkboxStatus: false,
+        }
+    },
+
+    //функция обработки текста с поля input 
+    inputTextChange: function (EO) {
+        this.changeStatelist(EO.target.value, this.state.checkboxStatus);
+    },
+
+    //функция обработки checkbox
+    checkboxСlicked: function (EO) {
+        this.changeStatelist(EO.target.checked, this.state.stateInputText);
+    },
+
+    //функция сортировки массива с учетом checkboxСlicked, inputTextChange
+    changeStatelist: function (inputText, checked) {
+        var newStateList = this.props.fullList.filter((v) => v.includes(inputText));//создание нового списка с проверкой на введенные символы
+
+        checked ? newStateList.sort() : null;
+
+        this.setState({ stateFullList: newStateList, stateInputText: inputText, checkboxStatus: checked, });
+    },
+
+
+    //
+    render: function () {
+        var displayList = this.state.stateFullList.map(function (v) {
+            return React.DOM.li({ key: v }, v)
+        });
+
+        return React.DOM.div({ className: "FullList" },
+            React.DOM.label(null, 'Cортировать',
+                React.DOM.input({ type: 'checkbox', onClick: this.checkboxСlicked }, null)),
+
+
+            React.DOM.input({ className: "text", type: 'text', onChange: this.inputTextChange }, null),
+
+            React.DOM.div({ className: "list" }, displayList)
+        );
+    }
 });
