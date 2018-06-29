d4da0887e2c8375a522f2af3efec3d7710d1196e
drahunpavel <drahunpavel@gmail.com>
2018-06-30 01:44:21 +0300

    1

--- br2jsx/src/components/App.js
+++ br2jsx/src/components/App.js
@@ -15,19 +15,21 @@ class App extends Component {
         let arr = text.split("<br>");
         arr.forEach((v, i) => {
             if (i > 0) {
-                newArr.push(<br key={i} />);
+                newArr.push(<br key={i} />);//перед каждым элементов массива становится разрыв строки
                 newArr.push(v);
+                //console.log(newArr);
             }
-            else newArr.push(v);
+            else newArr.push(v); // необязательно
         });
         return newArr;
     };
 
 
     render() {
-        console.log(this.props.children)
-        console.log(this.props.children.split('<br>'))
-        console.log(this.newFunc(this.props.children))
+        console.log(this.props.children);
+        console.log(this.props.children.split('<br>')); //split - ревратить строку в массив, разбив ее по разделителю <br>
+        console.log(this.newFunc(this.props.children));
+        
         return (
             <div>
                 <p>
