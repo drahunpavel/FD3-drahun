c69e0d4ad1a37bb75767e7e67a50199dbe4ad98b
drahunpavel <drahunpavel@gmail.com>
2018-06-29 08:28:00 +0300

    1

--- rainbowframe/src/index.js
+++ rainbowframe/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import RainbowFrame from './components/RainbowFrame'
 
-var text = "RainbowFrame";
-var colors = require('./src/colors.json');
+//var text = "RainbowFrame";
+var colors = require('./colors.json');
 
 render(<RainbowFrame>colors = { colors }</RainbowFrame>, document.getElementById('root'))
