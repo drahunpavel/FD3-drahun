b349b34ee7ec663c9427f935632221c7e6d1dd74
drahunpavel <drahunpavel@gmail.com>
2018-08-17 00:49:08 +0300

    111

--- Chat v2/index.js
+++ Chat v2/index.js
@@ -1,13 +1,10 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
-
+//import App from "./src/components/App";
+//import Appppp from './src/components/Appppp';
+import Appppp from "./src/components/Appppp";
 ReactDOM.render(
-<div>
-  hi!
-</div>
-  ,
+  <Appppp/>,
   document.getElementById('root')
 );
---
+++ Chat v2/src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//import './src/components-style/style.css';
+//import style from "./src/components-style/style";
+//import ProductCard from './ProductCard';
+//import ProductsGrid from './src/components/ProductsGrid';
+
+class Appppp extends React.Component {
+    render() {
+        return (
+            <div>
+            hi!
+        </div>
+            // <div id="container">
+            //     <aside id="sidebar">Users</aside>
+            //     <section id="main">
+            //         <section id="messages-list">Messages list</section>
+            //         <section id="new-message">New message</section>
+            //     </section>
+            // </div>
+        );
+    }
+};
+
+export default Appppp;
