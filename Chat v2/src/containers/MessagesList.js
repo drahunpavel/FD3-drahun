41aed8221d75081fc895d97c3d66b17cc457dd67
drahunpavel <drahunpavel@gmail.com>
2018-08-17 22:47:55 +0300

    added containers

---
+++ Chat v2/src/containers/MessagesList.js
@@ -0,0 +1,6 @@
+import { connect } from 'react-redux'
+import MessagesList from './src/components/MessagesList'
+
+export const MessagesList = connect(state => ({
+  messages: state.messages
+}), {})(MessagesListComponent)
