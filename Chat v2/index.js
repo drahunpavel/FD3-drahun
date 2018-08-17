578dcb58f50285b31cefa97962b5cbde31394501
drahunpavel <drahunpavel@gmail.com>
2018-08-17 22:21:39 +0300

    created and added registerServiceWorker.js

--- Chat v2/index.js
+++ Chat v2/index.js
@@ -7,15 +7,15 @@ import { createStore } from 'redux';
 import './src/components-style/style.css';
 import App from "./src/components/App";
 
-//import registerServiceWorker from './registerServiceWorker'
-//import chat from './reducers'
+import registerServiceWorker from './registerServiceWorker'
+import chat from './reducers'
 
-//const store = createStore(chat)
+const store = createStore(chat)
 
 ReactDOM.render(
-  //<Provider store={store}>
-    <App />,
-  //</Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
-//registerServiceWorker()
+registerServiceWorker()
