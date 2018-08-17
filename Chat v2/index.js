035c28d84e78119b63955e4d2ca9453d3e25ab6e
drahunpavel <drahunpavel@gmail.com>
2018-08-17 11:56:18 +0300

    replacement package

--- Chat v2/index.js
+++ Chat v2/index.js
@@ -1,18 +1,21 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './src/components-style/style.css';
 import App from "./src/components/App";
 
+//import registerServiceWorker from './registerServiceWorker'
+//import chat from './reducers'
 
-
-
+//const store = createStore(chat)
 
 ReactDOM.render(
-
-  <App />,
-  
-
+  //<Provider store={store}>
+    <App />,
+  //</Provider>,
   document.getElementById('root')
 );
+//registerServiceWorker()
