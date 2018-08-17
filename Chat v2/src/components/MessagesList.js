0ecb246ad4b77609347a337a65c3a97849b4e7a7
drahunpavel <drahunpavel@gmail.com>
2018-08-17 22:51:47 +0300

    added Message.js, created MessageList.js

--- Chat v2/src/components/MessagesList.js
+++ Chat v2/src/components/MessagesList.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import Message from "./Message";
 
+const MessagesList = ({ messages }) => (
+  <section id="messages-list">
+    <ul>
+    {messages.map(message => (
+      <Message
+      key={message.id}
+      {...message}
+      />
+    ))}
+    </ul>
+  </section>
+)
 
-
-class MessagesList extends React.Component {
-    render() {
-        return (
-            <section id="messages-list">Messages list</section>
-        );
-    }
-};
+MessagesList.propTypes = {
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+}
 
 export default MessagesList;
