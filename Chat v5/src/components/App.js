c45e6696f2bc99edcdb82ad0451727976e522fb4
drahunpavel <drahunpavel@gmail.com>
2018-09-10 20:45:23 +0300

    chatScreen

--- Chat v5/src/components/App.js
+++ Chat v5/src/components/App.js
@@ -1,34 +1,42 @@
 import React, { Component } from 'react'
 import UsernameForm from './UsernameForm'
+import ChatScreen from "./ChatsScreen"
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      currentUsername: '',
+    constructor() {
+        super()
+        this.state = {
+            currentUsername: '',
+            currentScreen: 'WhatIsYourUsernameScreen'
+        }
+        this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this)
     }
-    this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this)
-  }
 
-  onUsernameSubmitted(username) {
-    fetch('http://localhost:3001/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username }),
-    })
-      .then(response => {
-        this.setState({
-          currentUsername: username
+    onUsernameSubmitted(username) {
+        fetch('http://localhost:3001/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username }),
         })
-      })
-      .catch(error => console.error('error', error))
-  }
+            .then(response => {
+                this.setState({
+                    currentUsername: username,
+                    currentScreen: 'ChatScreen'
+                })
+            })
+            .catch(error => console.error('error', error))
+    }
 
-  render() {
-   return <UsernameForm onSubmit={this.onUsernameSubmitted} />
-  }
+    render() {
+        if (this.state.currentScreen === 'WhatIsYourUsernameScreen') {
+            return <UsernameForm onSubmit={this.onUsernameSubmitted} />
+        }
+        if (this.state.currentScreen === 'ChatScreen') {
+            return <ChatScreen currentUsername={this.state.currentUsername} />
+        }
+    }
 }
 
 export default App
--- Chat v5/src/components/ChatsScreen.js
+++ Chat v5/src/components/ChatsScreen.js
@@ -1,13 +1,89 @@
 import React, { Component } from 'react'
+import Chatkit from '@pusher/chatkit'
+import MessageList from './MessageList'
 
-class ChatScreen extends Component {  
-  render() {
-    return (
-      <div>
-        <h1>Chat</h1>
-      </div>
-    )
-  }
+
+class ChatScreen extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentUser: {},
+            currentRoom: {},
+            messages: []
+        }
+    }
+
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator: 'v1:us1:5e1af052-19c4-424c-ba3f-69494c8caa62',
+            userId: this.props.currentUsername,
+            tokenProvider: new Chatkit.TokenProvider({
+                url: 'http://localhost:3001/authenticate',
+            }),
+        })
+
+        chatManager
+            .connect()
+            .then(currentUser => {
+                this.setState({ currentUser })
+                return currentUser.subscribeToRoom({
+                    roomId: 15858912,
+                    messageLimit: 100,
+                    hooks: {
+                        onNewMessage: message => {
+                            this.setState({
+                                messages: [...this.state.messages, message],
+                            })
+                        },
+                    },
+                })
+            })
+            .then(currentRoom => {
+                this.setState({ currentRoom })
+            })
+            .catch(error => console.error('error', error))
+    }
+
+    render() {
+        const styles = {
+            container: {
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+            },
+            chatContainer: {
+                display: 'flex',
+                flex: 1,
+            },
+            whosOnlineListContainer: {
+                width: '300px',
+                flex: 'none',
+                padding: 20,
+                backgroundColor: '#2c303b',
+                color: 'white',
+            },
+            chatListContainer: {
+                padding: 20,
+                width: '85%',
+                display: 'flex',
+                flexDirection: 'column',
+            },
+        }
+        return (
+            <div style={styles.container}>
+                <div style={styles.chatContainer}>
+                    <aside style={styles.whosOnlineListContainer}>
+                        <h2>Who's online PLACEHOLDER</h2>
+                    </aside>
+                    <MessageList
+                        messages={this.state.messages}
+                        style={styles.chatList}
+                    />
+                </div>
+            </div>
+        )
+    }
 }
 
 export default ChatScreen
