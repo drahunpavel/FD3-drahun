13d8d2b2343df79289a51718e4589e8d5182f067
drahunpavel <drahunpavel@gmail.com>
2018-09-10 15:45:29 +0300

    11

--- Chat v4/src/Components/App.js
+++ Chat v4/src/Components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-
+import { ChatManager, TokenProvider } from '@pusher/chatkit'
 
 import "./App.css";
 import Title from "./Title";
 import MessageList from "./MessageList";
 import SendMessageForm from "./SendMessageForm";
+
+
+
+const instanceLocator = "v1:us1:5e1af052-19c4-424c-ba3f-69494c8caa62"
+const testToken ="https://us1.pusherplatform.io/services/chatkit_token_provider/v1/5e1af052-19c4-424c-ba3f-69494c8caa62/token"
+const username = "Anna"
+const roomId = 15838607
+
 
 class App extends React.Component {
 
@@ -22,13 +30,44 @@ class App extends React.Component {
 
     state={
         blankData:this.props.blank_data,
+        messages: []
     }
   
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator: instanceLocator,
+            userId: username,
+            tokenProvider: new Chatkit.TokenProvider({
+                url: testToken
+            })
+        })
+        
+        chatManager.connect().then(currentUser => {
+            this.currentUser = currentUser
+            this.currentUser.subscribeToRoom({
+            roomId: roomId,
+            hooks: {
+                onNewMessage: message => {
+
+                    this.setState({
+                        messages: [...this.state.messages, message]
+                    })
+                }
+            }
+        })
+      })
+    }
+
+
   render() {
     return (
       <div className="app">
         <Title />
-        <MessageList messages={this.state.blankData} />
+        <MessageList
+         //messages={this.state.blankData} 
+         roomId={this.state.roomId}
+         messages={this.state.messages}
+         />
         <SendMessageForm />
      </div>
     )
