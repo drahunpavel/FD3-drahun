026dea22f00660fd2e3c296da2fbac9dad120798
drahunpavel <drahunpavel@gmail.com>
2018-09-10 19:57:27 +0300

    ready authorization, create a chat window

--- Chat v5/server.js
+++ Chat v5/server.js
@@ -1,12 +1,41 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('pusher-chatkit-server')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:5e1af052-19c4-424c-ba3f-69494c8caa62',
+  key: 'dda3e45d-1b8c-487f-8c35-c5964f5174c6:A4hsp4hMuCso6m6df23buHXGANlmKgRax6Mnzocd65I=',
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  chatkit
+    .createUser({ 
+	id: username, 
+	name: username 
+     })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error_type === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.status(error.status).json(error)
+      }
+    })
+})
+
+app.post('/authenticate', (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id })
+  res.status(authData.status).send(authData.body)
+})
+
 
 const PORT = 3001
 app.listen(PORT, err => {
--- Chat v5/src/components/App.js
+++ Chat v5/src/components/App.js
@@ -1,9 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import UsernameForm from './UsernameForm'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentUsername: '',
+    }
+    this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this)
+  }
+
+  onUsernameSubmitted(username) {
+    fetch('http://localhost:3001/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username }),
+    })
+      .then(response => {
+        this.setState({
+          currentUsername: username
+        })
+      })
+      .catch(error => console.error('error', error))
+  }
+
   render() {
-    return <h1>Chatly</h1>
+   return <UsernameForm onSubmit={this.onUsernameSubmitted} />
   }
 }
 
-export default App;
+export default App
