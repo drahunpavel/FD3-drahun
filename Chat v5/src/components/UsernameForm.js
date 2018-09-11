6480a51a329200f2f9dd2c673c3d215b0e90e347
drahunpavel <drahunpavel@gmail.com>
2018-09-11 09:36:45 +0300

    live chat version

--- Chat v5/src/components/UsernameForm.js
+++ Chat v5/src/components/UsernameForm.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 
 class UsernameForm extends Component {
- constructor(props) {
-   super(props)
-   this.state = {
-     username: '',
-   }
-   this.onSubmit = this.onSubmit.bind(this)
-   this.onChange = this.onChange.bind(this)
- }
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+    }
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
 
- onSubmit(e) {
-   e.preventDefault()
-   this.props.onSubmit(this.state.username)
- }
+  onSubmit(e) {
+    e.preventDefault()
+    this.props.onSubmit(this.state.username)
+  }
 
- onChange(e) {
+  onChange(e) {
     this.setState({ username: e.target.value })
   }
 
@@ -23,7 +23,7 @@ class UsernameForm extends Component {
     return (
       <div>
         <div>
-          <h2>What is your username?</h2>
+          <h2>What is your usernane?</h2>
           <form onSubmit={this.onSubmit}>
             <input
               type="text"
@@ -38,4 +38,4 @@ class UsernameForm extends Component {
   }
 }
 
- export default UsernameForm
+export default UsernameForm
